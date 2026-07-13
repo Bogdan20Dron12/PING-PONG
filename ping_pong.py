9919057a86f6c7c6767f1955c188be6a3f554a88
Bogdan20Dron12 <b88884517@gmail.com>
2026-07-13 10:58:39 +0300

    add go ball

--- ping_pong.py
+++ ping_pong.py
@@ -33,7 +33,14 @@ window.fill((200, 255, 255))
 
 racket_left = Player('racket.png', 30, 200, 50, 150, 4)
 racket_right = Player('racket.png', 615, 200, 50, 160, 4)
+ball = GameSprate('tenis_ball.png', 200, 200, 50, 50, 5)
+font.init()
+font = font.Font(None, 70)
+loose_1 = font.render('PLAYER 1 LOOSER!!!', True, (255, 0, 0))
+loose_2 = font.render('PLAYER 2 LOOSER!!!', True, (255, 0, 0))
 
+speed_x = 5
+speed_y = 5
 clock = time.Clock()
 game = True
 finish = False
@@ -47,5 +54,19 @@ while game:
         racket_right.update_right()
         racket_left.reset()
         racket_right.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 450 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(racket_left, ball) or sprite.collide_rect(racket_right, ball):
+            speed_x *= -1
+        ball.reset()
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(loose_1, (200, 200))
+        if ball.rect.x > 650:
+            finish = True
+            window.blit(loose_2, (200, 200))    
+
     display.update()
     clock.tick(60)    
